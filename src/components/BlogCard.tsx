6ce6dad0948a8c33a2fcded335ab0f054e8629d0
Tarik <tguersoy@protonmail.com>
2025-02-26 06:17:20 +0100

    burgerbutton and fixes

--- src/components/BlogCard.tsx
+++ src/components/BlogCard.tsx
@@ -10,13 +10,24 @@ interface BlogCardProps {
 }
 
 export default function BlogCard({ title, description, image, slug, setCurrentPage, cardType }: BlogCardProps) {
+  const handleCardClick = () => {
+    setCurrentPage(`${cardType}-${slug}`);
+  };
+
   return (
     <Card className="blog-card h-100 rounded-4">
-      <Card.Img variant="top" src={image} className="blog-card-img" />
-      <Card.Body className="blog-card-body">
+      <Card.Img
+        variant="top"
+        src={image}
+        className="blog-card-img"
+        onClick={handleCardClick}
+        role="button"
+        aria-label={`Read more about ${title}`}
+      />
+      <Card.Body className="blog-card-body d-flex flex-column">
         <Card.Title className="blog-card-title">{title}</Card.Title>
-        <Card.Text className="blog-card-text">{description}</Card.Text>
-        <Button className="cyberpunk-button" onClick={() => setCurrentPage(`${cardType}-${slug}`)}>
+        <Card.Text className="blog-card-text text-truncate-3">{description}</Card.Text>
+        <Button className="cyberpunk-button mt-auto" onClick={handleCardClick}>
           Read More
         </Button>
       </Card.Body>
--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -3,29 +3,44 @@ import logo from "../../public/images/img_logo.png"; // Logo importieren
 
 export default function NavigationBar({ setCurrentPage }: { setCurrentPage: (page: string) => void }) {
   return (
-    <Navbar className="custom-navbar">
+    <Navbar className="custom-navbar" expand="lg">
       <Container>
-        {/* Navbar-Brand mit Logo */}
-        <Navbar.Brand 
-          onClick={() => setCurrentPage("home")} 
-          style={{ cursor: "pointer", display: "flex", alignItems: "center" }}
+        {/* Navbar-Brand mit Logo und Text nebeneinander */}
+        <Navbar.Brand
+          onClick={() => setCurrentPage("home")}
+          style={{
+            cursor: "pointer",
+            display: "flex",
+            alignItems: "center",
+            color: "#fff", // Optional, damit der Text weiß bleibt
+          }}
         >
-          <img 
-            src={logo} 
-            alt="Logo" 
-            style={{ width: 64, height: 64, borderRadius: "50%", marginRight: 10 }}
+          <img
+            src={logo}
+            alt="Logo"
+            style={{
+              width: 64,
+              height: 64,
+              borderRadius: "50%",
+              marginRight: 10,
+            }}
           />
           {"[./neuraldog]"}
         </Navbar.Brand>
 
+        {/* Hamburger Toggle für mobile Nutzer */}
+        <Navbar.Toggle className="cyberpunk-burger"/>
+        
         {/* Navigation-Links */}
-        <Nav className="ms-auto">
-          <Nav.Link className="nav-link" onClick={() => setCurrentPage("home")}>./Home</Nav.Link>
-          <Nav.Link className="nav-link" onClick={() => setCurrentPage("about")}>./About</Nav.Link>
-          <Nav.Link className="nav-link" onClick={() => setCurrentPage("projects")}>./Projects</Nav.Link>
-          <Nav.Link className="nav-link" onClick={() => setCurrentPage("skills")}>./Skills</Nav.Link>
-          <Nav.Link className="nav-link" onClick={() => setCurrentPage("blogs")}>./Blogs</Nav.Link>
-        </Nav>
+        <Navbar.Collapse id="navbar-nav">
+          <Nav className="ms-auto">
+            <Nav.Link className="nav-link" onClick={() => setCurrentPage("home")}>./Home</Nav.Link>
+            <Nav.Link className="nav-link" onClick={() => setCurrentPage("about")}>./About</Nav.Link>
+            <Nav.Link className="nav-link" onClick={() => setCurrentPage("projects")}>./Projects</Nav.Link>
+            <Nav.Link className="nav-link" onClick={() => setCurrentPage("skills")}>./Skills</Nav.Link>
+            <Nav.Link className="nav-link" onClick={() => setCurrentPage("blogs")}>./Blogs</Nav.Link>
+          </Nav>
+        </Navbar.Collapse>
       </Container>
     </Navbar>
   );
